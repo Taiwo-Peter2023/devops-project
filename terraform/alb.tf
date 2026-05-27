2a984b18e76e3762221c4f8bfc4f8ea8cef5c897
Taiwo Peter Olatunji <taiwopeter2018@gmail.com>
2026-05-27 22:18:07 +0100

    alb file update

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -29,12 +29,8 @@ resource "aws_lb_listener" "http" {
 
 
   default_action {
-    type = "redirect"
-
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
+    type    = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
   }
 }
+  
