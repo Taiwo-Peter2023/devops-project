d40c47e27247cd7b2dc525302b191292bab4203f
Taiwo Peter Olatunji <taiwopeter2018@gmail.com>
2026-05-27 21:58:11 +0100

    change the entire https file to comment

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -22,11 +22,11 @@ resource "aws_lb_target_group" "app_tg" {
   }
 }
 
-
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.main.arn
   port              = 80
   protocol          = "HTTP"
+
 
   default_action {
     type = "redirect"
