b0d0ce34a95441aed6bac1eed4a871adf12b690b
Taiwo Peter Olatunji <taiwopeter2018@gmail.com>
2026-05-27 18:59:17 +0100

    port 443 added

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,26 +1,27 @@
-resource "aws_lb" "main" {
-  name               = "devops-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.ecs_sg.id]
-  subnets = [
-    aws_subnet.public_1.id,
-    aws_subnet.public_2.id
-  ]
-}
+# resource "aws_lb" "main" {
+#   name               = "devops-alb"
+#   internal           = false
+#   load_balancer_type = "application"
+#   security_groups    = [aws_security_group.ecs_sg.id]
+#   subnets = [
+#     aws_subnet.public_1.id,
+#     aws_subnet.public_2.id
+#   ]
+# }
 
-resource "aws_lb_target_group" "app_tg" {
-  name        = "devops-target-group"
-  port        = 5000
-  protocol    = "HTTP"
-  target_type = "ip"
-  vpc_id      = aws_vpc.main.id
+# resource "aws_lb_target_group" "app_tg" {
+#   name        = "devops-target-group"
+#   port        = 5000
+#   protocol    = "HTTP, HTTPS"
+#   target_type = "ip"
+#   vpc_id      = aws_vpc.main.id
 
-  health_check {
-    path = "/"
-    protocol = "HTTP"
-  }
-}
+#   health_check {
+#     path = "/"
+#     protocol = "HTTP"
+#   }
+# }
+
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.main.arn
